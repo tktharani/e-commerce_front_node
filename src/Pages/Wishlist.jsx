36aad7233d345df9334317bae025db5b6dbe4fa9
Tharani T K <161691040+tktharani@users.noreply.github.com>
2024-08-12 13:43:14 +0530

    image ,username,id in wishlistpage

--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,39 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-const API_URL = 'http://localhost:5000'; 
 
 const WishlistPage = () => {
   const [wishlistItems, setWishlistItems] = useState([]);
+  const [user, setUser] = useState({});
 
   useEffect(() => {
     const fetchWishlistItems = async () => {
       try {
         const userId = localStorage.getItem('userId');
-        const response = await axios.get(`${API_URL}/wishlist/add`, {
+        const response = await axios.get(`http://localhost:5000/wishlist/${userId}`, {
           headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
           },
-          params: { userId }
         });
-        setWishlistItems(response.data);
+        setWishlistItems(response.data.products); // Assuming response.data.products contains the list of products in the wishlist
+        setUser(response.data.user);
       } catch (error) {
         console.error('Error fetching wishlist items:', error);
       }
     };
+
     fetchWishlistItems();
   }, []);
 
   return (
     <div className="container mt-5">
       <h1>My Wishlist</h1>
+      {user && (
+        <div className="mb-4">
+          <h5>User ID: {user._id}</h5>
+          <h5>Username: {user.username}</h5>
+        </div>
+      )}
+
       <ul className="list-group">
         {wishlistItems.map(item => (
           <li key={item._id} className="list-group-item">
-            {/* Display wishlist item details */}
             <div>
-              <h5>{item.product.name}</h5>
-              <p>Price: Rs.{item.product.price}</p>
-              <p>Category: {item.product.category}</p>
+              <h5>{item.name}</h5>
+              <p>Price: Rs.{item.price}</p>
+              <p>Category: {item.category}</p>
+              <img src={`http://localhost:5000/public/data/uploads/${item.image}`} 
+              className="card-img-top" alt={item.name} style={{ width: '50px', height: '50px', objectFit: 'cover' }} />
+   
+        
             </div>
           </li>
         ))}
